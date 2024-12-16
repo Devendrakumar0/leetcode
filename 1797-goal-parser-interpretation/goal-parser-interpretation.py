f40a7da87bf44f9406108b529ea69c4c06365e7e
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2024-12-16 14:49:58 +0530

    Time: 27 ms (93.45%) | Memory: 17.2 MB (15.68%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,15 +1,6 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        x_list = list(command)
-        i=0
-        while i<len(x_list):
-            if x_list[i]=="(" and i + 1 < len(x_list) and x_list[i+1]==")":
-                x_list[i]="o"
-                x_list.pop(i+1)
-            elif x_list[i] == "(" or x_list[i]==")":
-                x_list.pop(i)
-            else:
-                i+=1
-        command = ''.join(x_list)
+        command = command.replace("()","o")
+        command = command.replace("(","").replace(")","")
         return command
         
