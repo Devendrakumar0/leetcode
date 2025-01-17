40ef8e03ebcc47b16a124f31c324b6de43c6d4cc
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2025-01-17 13:22:42 +0530

    Time: 40 ms (72.99%) | Memory: 22.5 MB (24.82%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -1,7 +1,6 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        derived_sum = 0
-        for num in derived:
-            derived_sum ^= num
-        return derived_sum == 0
+        from functools import reduce
+        from operator import xor
+        return reduce(xor, derived) == 0
         
