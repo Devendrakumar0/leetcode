77a9be505203613e3789c4ba6114a42b5cb8f166
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2025-01-25 23:00:33 +0530

    Time: 4 ms (57.89%) | Memory: 19 MB (73.14%) - LeetSync

--- 1839-decode-xored-array/decode-xored-array.py
+++ 1839-decode-xored-array/decode-xored-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
-        r = []
-        r.append(first)
-        for i in range(1, len(encoded)+1):
-            r.append(r[i-1] ^ encoded[i-1])
+        r = [first]
+        for i in encoded:
+            r.append(r[-1] ^ i)
         return r
 
         
