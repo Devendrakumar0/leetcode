984c3752be26bd4fab9aa58672cc780206e7b622
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2025-02-25 02:18:10 +0530

    Time: 2 ms (84.58%) | Memory: 46 MB (46.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[n];
-        int index = 0;
-        for(int i = 0;i<n;i++){
-            if(nums[i]!=0) temp[index++] = nums[i];
+        int j = -1;
+        for(int i = 0; i<n; i++){
+            if(nums[i]==0) {
+                j = i;
+                break;
+            }
         }
-        for(int i = 0;i<temp.length;i++){
-            nums[i] = temp[i];
-        }
-        for(int i = temp.length;i<n;i++){
-            nums[i] = 0;
+        if(j==-1) return;
+        for(int i = j+1; i<n; i++){
+            if(nums[i]!=0){
+                reverse(nums, i, j);
+                j++;
+            }
         }
     }
-}
+        private void reverse(int[] nums, int start, int end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+        }
+    }
+    
