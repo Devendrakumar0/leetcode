f8c348819ddc86673fb1eb32bd32153bf3a2d573
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2025-03-01 16:24:19 +0530

    Time: 1 ms (96.37%) | Memory: 44.1 MB (22.29%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -1,35 +1,21 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        int j = 0;
-        for(int i=1; i<nums.length; i++){
-            if(nums.length == 2 && nums[i]!=nums[j] && nums[i]!=0 && nums[j]!=0) return nums;
-            if(nums[i] == nums[j] && nums[i]!=0 && nums[j]!=0){
-                nums[j] = nums[j]*2;
-                nums[i] = 0;
-                j++;
-            }
-            else{
-                j++;
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
         }
-        int k = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]==0){
-                k = i;
-                break;
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
             }
         }
-        for(int i = k+1; i<nums.length; i++){
-            if(nums[i]!=0){
-                reverse(nums, i, k);
-                k++;
-            }
+        while (index < n) {
+            nums[index++] = 0;
         }
         return nums;
     }
-    public void reverse(int[] nums, int i, int j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
 }
