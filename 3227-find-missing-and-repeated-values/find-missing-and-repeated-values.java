40b5be5e1e709cb78ed808773e06cc165e920289
Devendra Kumar <84584733+Devendrakumar0@users.noreply.github.com>
2025-03-06 22:18:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (14.68%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,39 +1,23 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int newLength = (int)Math.pow(grid.length, 2);
-        int[] res = new int[newLength];
+        int n = grid.length;
+        int N = n * n;
         int[] result = new int[2];
-        int k = 0;
-        for(int i = 0;i<grid.length;i++){
-            for(int j = 0; j<grid[i].length;j++){
-                if(check(res, grid[i][j])==false){
-                    result[0] = grid[i][j];
+        boolean[] seen = new boolean[N + 1];
+        for (int[] row : grid) {
+            for (int num : row) {
+                if (seen[num]) {
+                    result[0] = num;
                 }
-                else{
-                    res[k++] = grid[i][j];
-                }
+                seen[num] = true;
             }
         }
-        TreeSet<Integer> Uniq = new TreeSet<>();
-        for(int i:res){
-            Uniq.add(i);
-        }
-        
-        Integer[] arr = Uniq.toArray(new Integer[0]);
-        for(int i: arr){
-            System.out.print(i + " ");
-        }
-        for(int i=0; i<res.length;i++){
-            if((i+1)!=arr[i]){
-                result[1] = arr[i]+1;
+        for (int i = 1; i <= N; i++) {
+            if (!seen[i]) {
+                result[1] = i;
+                break;
             }
         }
         return result;
     }
-    public boolean check(int[] res, int key){
-            for(int i:res){
-                if(i==key) return false;
-            }
-            return true;
-        }
 }
